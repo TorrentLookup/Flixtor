8ce7afde00913fe3cc6e98af2ba79439244b8c52
Japoilski <fredcharette@torrentlookup.com>
2014-04-25 01:00:24 -0400

    New language list :D We need them all.

---
+++ js/localization.js
@@ -0,0 +1,195 @@
+var localization = function() {
+
+    var localizator = {};
+
+    localizator.languages = {
+        "af" : {
+            lang : "Afrikaans",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "sq" : {
+            lang : "Albanian",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "ar" : {
+            lang : "Arabic",
+            encoding : [ "iso-8859-6", "windows-1256" ]
+        },
+        "bat" : {
+            lang : "Baltic",
+            encoding : [ "iso-8859-4", "iso-8859-13", "windows-1257" ]
+        },
+        "eu" : {
+            lang : "Basque",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "bg" : {
+            lang : "Bulgarian",
+            encoding : [ "iso-8859-5" ]
+        },
+        "be" : {
+            lang : "Byelorussian",
+            encoding : [ "iso-8859-6" ]
+        },
+        "ca" : {
+            lang : "Catalan",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "hr" : {
+            lang : "Croatian",
+            encoding : [ "iso-8859-2", "windows-1250" ]
+        },
+        "cs" : {
+            lang : "Czech",
+            encoding : [ "iso-8859-2" ]
+        },
+        "da" : {
+            lang : "Danish",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "nl" : {
+            lang : "Dutch",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "en" : {
+            lang : "English",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "eo" : {
+            lang : "Esperanto",
+            encoding : [ "iso-8859-3" ]
+        },
+        "et" : {
+            lang : "Estonian",
+            encoding : [ "iso-8859-15" ]
+        },
+        "fo" : {
+            lang : "Faroese",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "fi" : {
+            lang : "Finnish",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "fr" : {
+            lang : "French",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "gl" : {
+            lang : "Galician",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "de" : {
+            lang : "German",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "el" : {
+            lang : "Greek",
+            encoding : [ "iso-8859-7", "windows-1253" ]
+        },
+        "iw" : {
+            lang : "Hebrew",
+            encoding : [ "iso-8859-8", "windows-1255" ]
+        },
+        "hu" : {
+            lang : "Hungarian",
+            encoding : [ "iso-8859-2" ]
+        },
+        "is" : {
+            lang : "Icelandic",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "ga" : {
+            lang : "Irish",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "it" : {
+            lang : "Italian",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "ja" : {
+            lang : "Japanese",
+            encoding : [ "shift_jis", "iso-2022-jp", "euc-jp" ]
+        },
+        "ko" : {
+            lang : "Korean",
+            encoding : [ "euc-kr" ]
+        },
+        "lv" : {
+            lang : "Latvian",
+            encoding : [ "iso-8859-13", "windows-1257" ]
+        },
+        "lt" : {
+            lang : "Lithuanian",
+            encoding : [ "iso-8859-13", "windows-1257" ]
+        },
+        "mk" : {
+            lang : "Macedonian",
+            encoding : [ "iso-8859-5", "windows-1251" ]
+        },
+        "mt" : {
+            lang : "Maltese",
+            encoding : [ "iso-8859-3" ]
+        },
+        "no" : {
+            lang : "Norwegian",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "pl" : {
+            lang : "Polish",
+            encoding : [ "iso-8859-2" ]
+        },
+        "pt" : {
+            lang : "Portuguese",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "ro" : {
+            lang : "Romanian",
+            encoding : [ "iso-8859-16" ]
+        },
+        "ru" : {
+            lang : "Russian",
+            encoding : [ "koi8-r", "iso-8859-5" ]
+        },
+        "gd" : {
+            lang : "Scottish",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "sr" : {
+            lang : "Serbian cyrillic",
+            encoding : [ "windows-1251", "iso-8859-5" ]
+        },
+        "sr" : {
+            lang : "Serbian latin",
+            encoding : [ "iso-8859-2", "windows-1250" ]
+        },
+        "sk" : {
+            lang : "Slovak",
+            encoding : [ "iso-8859-2" ]
+        },
+        "sl" : {
+            lang : "Slovenian",
+            encoding : [ "iso-8859-2", "windows-1250" ]
+        },
+        "es" : {
+            lang : "Spanish",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "sv" : {
+            lang : "Swedish",
+            encoding : [ "iso-8859-1", "windows-1252" ]
+        },
+        "tr" : {
+            lang : "Turkish",
+            encoding : [ "iso-8859-9", "windows-1254" ]
+        },
+        "uk" : {
+            lang : "Ukrainian",
+            encoding : [ "iso-8859-5" ]
+        }
+    }
+
+    return localizator;
+}
+
+module.exports = localization;
