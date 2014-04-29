6f409bef2aa4feccc51a5d1c614e768c0465ab3b
Japoilski <fredcharette@torrentlookup.com>
2014-04-28 21:45:58 -0400

    Serie player is done. I have maybe 2days of work until we got something flawless and smooth. Need to add subtitles and VLC support.

--- js/callwcf.js
+++ js/callwcf.js
@@ -9,43 +9,43 @@ var searchTable = function (tableName, searchKey, searchValue, ext, sorColumn, s
     }
 
     var input = parseSearch(searchKey, searchValue);
-        var BasicHttpBinding = require('wcf.js').BasicHttpBinding,
-            Proxy = require('wcf.js').Proxy,
-            binding = new BasicHttpBinding({
-                SecurityMode: "None"
-            }),
-            proxy = new Proxy(binding, "http://www.torrentlookup.com/services/v4/services/extensionservices/searchservice.svc"),
-            message = "<Envelope xmlns='http://schemas.xmlsoap.org/soap/envelope/'>" +
-                "<Header />" +
-                "<Body>" +
-                "<JSONSearch xmlns='http://tempuri.org/'>" +
-                "<tableName>" + tableName + "</tableName>" +
-                "<input>" + input + " " + ext + "</input>" +
-                "<skip>" + skip + "</skip>" +
-                "<max>" + max + "</max>" +
-                "<total>0</total>" +
-                "<sortColumn>" + sorColumn + "</sortColumn>" +
-                "<sortReverse>" + sortReverse + "</sortReverse>" +
-                "<sortType>" + sortTypeId + "</sortType>" +
-                "<randomSort>" + randomSort + "</randomSort>" +
-                "<randomSeed>" + randomSeed + "</randomSeed>" +
-                "</JSONSearch>" +
-                "</Body>" +
-                "</Envelope>"
-        proxy.send(message, "http://tempuri.org/ISearchService/JSONSearch", function (response, ctx) {
-            try{
+    var BasicHttpBinding = require('wcf.js').BasicHttpBinding,
+        Proxy = require('wcf.js').Proxy,
+        binding = new BasicHttpBinding({
+            SecurityMode: "None"
+        }),
+        proxy = new Proxy(binding, "http://www.torrentlookup.com/services/v4/services/extensionservices/searchservice.svc"),
+        message = "<Envelope xmlns='http://schemas.xmlsoap.org/soap/envelope/'>" +
+        "<Header />" +
+        "<Body>" +
+        "<JSONSearch xmlns='http://tempuri.org/'>" +
+        "<tableName>" + tableName + "</tableName>" +
+        "<input>" + input + " " + ext + "</input>" +
+        "<skip>" + skip + "</skip>" +
+        "<max>" + max + "</max>" +
+        "<total>0</total>" +
+        "<sortColumn>" + sorColumn + "</sortColumn>" +
+        "<sortReverse>" + sortReverse + "</sortReverse>" +
+        "<sortType>" + sortTypeId + "</sortType>" +
+        "<randomSort>" + randomSort + "</randomSort>" +
+        "<randomSeed>" + randomSeed + "</randomSeed>" +
+        "</JSONSearch>" +
+        "</Body>" +
+        "</Envelope>"
+    proxy.send(message, "http://tempuri.org/ISearchService/JSONSearch", function (response, ctx) {
+        try{
             var results = $(response).find("JSONSearchResult").text();
             var total = $(response).find("Total").text();
             results = results.replace(/(\r\n|\n|\r)/gm, "");
             var jsonResult = JSON.parse(results);
             jsonResult.Total = total;
             callback(jsonResult);
-            }catch(error)
-            {
-                utilities.showMsg("Error","Flixtor services are unavailable at the moment, please try again later. You can follow us on facebook to find out when the service will be online. <a target='blank' href='http://www.facebook.com/flixtorapp'>http://www.facebook.com/flixtorapp</a>");
-                callback("error");
-            }
-        });
+        }catch(error)
+        {
+            utilities.showMsg("Error","Flixtor services are unavailable at the moment, please try again later. You can follow us on facebook to find out when the service will be online. <a target='blank' href='http://www.facebook.com/flixtorapp'>http://www.facebook.com/flixtorapp</a>");
+            callback("error");
+        }
+    });
 }
 
 var getSimpleTopSerieTorrents = function (serieId, callback) {
@@ -56,13 +56,13 @@ var getSimpleTopSerieTorrents = function (serieId, callback) {
         }),
         proxy = new Proxy(binding, "http://www.torrentlookup.com/services/v4/services/torrentservices/torrentservice.svc"),
         message = "<Envelope xmlns='http://schemas.xmlsoap.org/soap/envelope/'>" +
-            "<Header />" +
-            "<Body>" +
-            "<JSONSimpleTopSerieTorrents xmlns='http://tempuri.org/'>" +
-            "<serieId>" + serieId + "</serieId>" +
-            "</JSONSimpleTopSerieTorrents>" +
-            "</Body>" +
-            "</Envelope>";
+        "<Header />" +
+        "<Body>" +
+        "<JSONSimpleTopSerieTorrents xmlns='http://tempuri.org/'>" +
+        "<serieId>" + serieId + "</serieId>" +
+        "</JSONSimpleTopSerieTorrents>" +
+        "</Body>" +
+        "</Envelope>";
 
     proxy.send(message, "http://tempuri.org/ITorrentService/JSONSimpleTopSerieTorrents", function (response, ctx) {
         var results = $(response).find("JSONSimpleTopSerieTorrentsResult").text();
@@ -104,9 +104,37 @@ var getGenres = function (id) {
     });
 }
 
+var getEpisodeTorrents = function (episodeId, max, callback) {
+    var BasicHttpBinding = require('wcf.js').BasicHttpBinding,
+        Proxy = require('wcf.js').Proxy,
+        binding = new BasicHttpBinding({
+            SecurityMode: "None"
+        }),
+        proxy = new Proxy(binding, "http://www.torrentlookup.com/services/v4/services/torrentservices/torrentservice.svc"),
+        message = "<Envelope xmlns='http://schemas.xmlsoap.org/soap/envelope/'>" +
+        "<Header />" +
+        "<Body>" +
+        "<JSONEpisodeTorrents xmlns='http://tempuri.org/'>" +
+        "<episodeId>" + episodeId + "</episodeId>" +
+        "<max>" + max + "</max>" +
+        "</JSONEpisodeTorrents>" +
+        "</Body>" +
+        "</Envelope>";
+
+    proxy.send(message, "http://tempuri.org/ITorrentService/JSONEpisodeTorrents", function (response, ctx) {
+        var results = $(response).find("JSONEpisodeTorrentsResult").text();
+        results = results.replace(/(\r\n|\n|\r)/gm, "");
+
+        var jsonResult = JSON.parse(results);
+
+        callback(jsonResult);
+    });
+}
+
 //Exports
 module.exports.getGenres = getGenres;
 module.exports.parseSearch = parseSearch;
 module.exports.getSimpleTopSerieTorrents = getSimpleTopSerieTorrents;
+module.exports.getEpisodeTorrents = getEpisodeTorrents;
 module.exports.searchTable = searchTable;
 module.exports.reloadInstance = reloadInstance;
